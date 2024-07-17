1488579d07445270c4b9a3daa2113cc28c0da34c
CHEGEBB <chegephil24@gmail.com>
2024-07-17 12:15:04 +0300

    chore: Update npm dependencies for fontawesome-svg-core, free-brands-svg-icons, and react-fontawesome

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -1,17 +1,51 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../sass/SignUp.scss";
+
+library.add(fab);
+
 const SignUp = () => {
-    return ( 
-        <div className="signup-container">
-            <h1>Sign Up</h1>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Confirm Password" />
-                <button type="submit">Sign Up</button>
-            </form>
-            <p>Already have an account? <a href="login">Login</a></p>
+  return (
+    <div className="signup-container">
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button type="submit">Sign Up</button>
+        <div className="socials-container">
+          <div className="social-icons">
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "instagram"]} size="2x" />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "facebook"]} size="2x" />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" />
+            </a>
+          </div>
         </div>
-     );
-}
- 
+      </form>
+      <p>
+        Already have an account? <a href="login">Login</a>
+      </p>
+    </div>
+  );
+};
+
 export default SignUp;
