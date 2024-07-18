48c16a4a4a14185c2be60b55c8729b66725d947b
CHEGEBB <chegephil24@gmail.com>
2024-07-18 18:24:10 +0300

    chore: Add Worksphere branding and admin profile to Sidenav component

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDashboard, faUserGroup, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faDashboard, faUserGroup, faChevronLeft, faChevronRight,faBriefcase,faCalendar,faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 import '../sass/Sidenav.scss';
 
 const Sidenav = ({ collapsed, toggleSidebar }) => {
   return (
     <div className={`sidenav ${collapsed ? 'collapsed' : ''}`}>
+    <div className="top">
+    <div className="brand-logo">
+        <h1>Worksphere</h1>
+    </div>
+    <div className="admin-profile">
+      <img src="https://via.placeholder.com/150" alt="Admin" />
+      <div className="admin-name">
+        <h3>Admin</h3>
+        <p>User Role</p>
+      </div>
+    </div>
+    </div>
       <div className="nav-items">
         <div className="nav-item">
           <div className="nav-icon">
@@ -19,6 +31,32 @@ const Sidenav = ({ collapsed, toggleSidebar }) => {
             <FontAwesomeIcon icon={faUserGroup} />
           </div>
           <Link to="/employees">Employees</Link>
+        </div>
+        <div className="nav-item">
+        <div className="nav-icon">
+            <FontAwesomeIcon icon={faBriefcase} />
+          </div>
+          <Link to="/projects">Projects</Link>
+        </div>
+        <div className="nav-item">
+          <div className="nav-icon">
+            <FontAwesomeIcon icon={faCalendar} />
+          </div>
+          <Link to="/attendance">Attendance</Link>
+        </div>
+        <div className="nav-item">
+          {/* leave management fa open door*/}
+          <div className="nav-icon">
+            <FontAwesomeIcon icon={faDoorOpen} />
+            </div>
+            <Link to="/leavemanagement">Leave Management</Link>
+          
+        </div>
+        <div className="nav-item">
+          <div className="nav-icon">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
+          <Link to="/">Logout</Link>
         </div>
       </div>
       <div className="toggle-sidebar" onClick={toggleSidebar}>
