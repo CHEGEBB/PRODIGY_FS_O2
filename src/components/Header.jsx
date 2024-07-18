6f5bec255ed8ba800b61c27aa7ea999170fd9414
CHEGEBB <chegephil24@gmail.com>
2024-07-18 23:07:51 +0300

    chore: Update header component with schedule, notifications, and messages icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,38 @@
 import "../sass/Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons/faCalendar";
 const Header = () => {
     return ( 
         <div className="header-container">
-            <div className="header-left">
-                <h1>Employee Management System</h1>
+           <div className="inform">
+            {/* will have calendar or schedules,messages and notifications */}
+           <div className="schedule">
+            <div className="schedule-icon">
+                <FontAwesomeIcon icon={faCalendar} />
             </div>
-            <div className="header-right">
-                <div className="header-notification">
-                    <FontAwesomeIcon icon={faBell} />
-                </div>
+            <div className="schedule-text">
+                <p>Schedule</p>
+ </div>
+           </div>
+           <div className="notify">
+            <div className="notify-icon">
+                <FontAwesomeIcon icon={faBell} />
             </div>
+            <div className="notify-text">
+                <p>Notifications</p>
+                 </div>
+           </div>
+           <div className="message">
+            <div className="message-icon">
+                <FontAwesomeIcon icon={faMessage} />
+            </div>
+            <div className="message-text">
+                <p>Messages</p>
+      </div>
+           </div>
+           </div>
         </div>
 
      );
