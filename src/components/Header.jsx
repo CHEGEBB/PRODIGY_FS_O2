b354d9235911f134f43652e53ccb6830b1576283
CHEGEBB <chegephil24@gmail.com>
2024-07-18 23:31:43 +0300

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import "../sass/Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
-import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faMessage} from "@fortawesome/free-solid-svg-icons";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons/faCalendar";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 const Header = () => {
     return ( 
         <div className="header-container">
+        <div className="dash">
+            {/* the dashboard icon and the name dashboard */}
+            <div className="dash-icon">
+                <FontAwesomeIcon icon={faHome} />
+                <p>Dashboard</p>
+                 </div>
+        </div>
+        <div className="search-field">
+        {/* will have search bar  with a fasearch icon*/}
+        <FontAwesomeIcon icon={faSearch} />
+        <input type="text" className="search" placeholder="Search here.." />
+        </div>
            <div className="inform">
             {/* will have calendar or schedules,messages and notifications */}
            <div className="schedule">
