39d6717feabc17f09f102fab208e888c200395de
CHEGEBB <chegephil24@gmail.com>
2024-07-20 18:23:13 +0300

    feat: Add form to add employees in AddEmployees page

--- frontend/src/pages/AddEmployees.jsx
+++ frontend/src/pages/AddEmployees.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import "../sass/AddEmployee.scss"
 const AddEmployee = () => {
     return (  
       <div className="add-employee-container">
@@ -7,6 +8,19 @@ const AddEmployee = () => {
         </div>
         <div className="add-main-container">
             {/* form to add employees */}
+            
+            <form className='add-employee'>
+            <div className="names">
+            <div className='form-group'>
+        <input type='text' id='firstName' required />
+        <label htmlFor='firstName'>First Name</label>
+      </div>
+      <div className='form-group'>
+        <input type='text' id='lastName' required />
+        <label htmlFor='lastName'>Last Name</label>
+        </div>
+      </div>
+            </form>
             
         </div>
       </div>
